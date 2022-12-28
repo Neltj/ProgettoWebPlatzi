15aa16ac89383c602d691d317c725a56e7f1379e
Neltj <f.malca@hotmail.com>
2022-12-29 00:53:13 +0100

    Aggiunta del menu-mobile

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const navEmail = document.querySelector('.navEmail')
 const desktopMenu = document.querySelector('.desktop-menu')
-
+const burgerMenu = document.querySelector('.menu-hamburger')
+const mobileMenu = document.querySelector('.mobile-menu')
+const logoMenu = document.querySelector('.logo-img')
+const boxBlue = document.querySelector('.box')
 
 navEmail.addEventListener('click', ToggleDesktopMenu)
+burgerMenu.addEventListener('click', ToggleBurgerMenu)
 
 function ToggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive') 
+    console.log('hai cliccato su email')
 }
+
+function ToggleBurgerMenu () {
+    mobileMenu.classList.toggle('inactive')
+    console.log('hai cliccato sul burger')
+}
+
