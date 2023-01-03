9ddc3544d128d98051b6dbd2f1fd08e7d89f36d7
Neltj <f.malca@hotmail.com>
2023-01-03 22:35:01 +0100

    Aggiunta di product detail

--- script.js
+++ script.js
@@ -2,19 +2,48 @@ const navEmail = document.querySelector('.navEmail')
 const desktopMenu = document.querySelector('.desktop-menu')
 const burgerMenu = document.querySelector('.menu-hamburger')
 const mobileMenu = document.querySelector('.mobile-menu')
-const logoMenu = document.querySelector('.logo-img')
-const boxBlue = document.querySelector('.box')
+const navImgCart = document.querySelector('.imgCart')
+const myOrder = document.querySelector('.my-order')
+
+
+
 
 navEmail.addEventListener('click', ToggleDesktopMenu)
 burgerMenu.addEventListener('click', ToggleBurgerMenu)
+navImgCart.addEventListener('click', ToggleMyOrder)
 
-function ToggleDesktopMenu () {
-    desktopMenu.classList.toggle('inactive') 
+function ToggleDesktopMenu () { 
     console.log('hai cliccato su email')
+    const isMyOrderClosed = myOrder.classList.contains('inactive')
+
+    if (!isMyOrderClosed) {
+        myOrder.classList.add('inactive')
+    }
+
+    desktopMenu.classList.toggle('inactive')
 }
 
 function ToggleBurgerMenu () {
-    mobileMenu.classList.toggle('inactive')
     console.log('hai cliccato sul burger')
+    const isMyOrderClosed = myOrder.classList.contains('inactive')
+
+    if (!isMyOrderClosed) {
+        myOrder.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive')
 }
 
+function ToggleMyOrder () {
+    console.log('Hai cliccato sul carrello')
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopMenuCLosed = desktopMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClosed || !isDesktopMenuCLosed) {
+        mobileMenu.classList.add('inactive')
+        desktopMenu.classList.add('inactive')
+    }
+
+    myOrder.classList.toggle('inactive')
+
+}
