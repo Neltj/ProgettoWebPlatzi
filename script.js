b4c871cfacf02b8c6a00e46ddd2b07f6fa957c2d
Neltj <f.malca@hotmail.com>
2023-01-06 16:13:32 +0100

    Aggiunti i prodotti tramite JS

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const burgerMenu = document.querySelector('.menu-hamburger')
 const mobileMenu = document.querySelector('.mobile-menu')
 const navImgCart = document.querySelector('.imgCart')
 const myOrder = document.querySelector('.my-order')
+const cardsContainer = document.querySelector('.cards-container')
 
 
 
@@ -47,3 +48,73 @@ function ToggleMyOrder () {
     myOrder.classList.toggle('inactive')
 
 }
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'img/bike.jpg'
+})
+
+productList.push({
+    name: 'Schermo',
+    price: 220,
+    image: 'img/bike.jpg'
+})
+
+productList.push({
+    name: 'Computer',
+    price: 890,
+    image: 'img/bike.jpg'
+})
+
+/*
+<div class="product-card">
+                <img src="img/bike.jpg" alt="" class="product-img">
+                <div class="product-info">
+                    <div class="product-description">
+                        <p>$120,00</p>
+                        <p>Bike</p>
+                    </div> 
+                    
+                    <figure>
+                        <img src="icons/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>
+*/
+
+
+
+function renderProducts (arr) {
+
+    for (product of arr) {
+
+        const productCard = document.createElement('div')
+        const productImage = document.createElement('img')
+        const productInfo = document.createElement('div')
+        const productDescription = document.createElement('div')
+        const productPrice = document.createElement('p')
+        const productName = document.createElement('p')
+        const figureContainer = document.createElement('figure')
+        const figureIconCart = document.createElement('img')
+        
+        productCard.classList.add('product-card')
+        productImage.setAttribute('src', product.image)
+        productInfo.classList.add('product-info')
+        productDescription.classList.add('product-description')
+        productPrice.innerText = '$'+ product.price
+        productName.innerText = product.name
+        figureIconCart.setAttribute('src', 'icons/bt_add_to_cart.svg')
+        productDescription.append(productPrice, productName)
+        productInfo.append(productDescription, figureContainer)
+        figureContainer.appendChild(figureIconCart)
+        productCard.append(productImage, productInfo)
+        cardsContainer.appendChild(productCard)
+    
+    }
+}
+
+renderProducts(productList)
+
